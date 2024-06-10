545c07e9b3e0bd157c9f570f7c824ad48c16b73a
chaitu1 <chaitu143@gmail.com>
2024-06-10 19:58:03 +0530

    Ansible expense setup is ready

---
+++ volumes/create-volumes.sh
@@ -0,0 +1,10 @@
+# To create to 3 volumes
+for i in {1..3}
+do
+  aws ec2 create-volume \
+      --availability-zone ap-south-1a \
+      --no-encrypted \
+      --volume-type gp2 \
+      --size 2 \
+      --tag-specifactions "ResourceType=volume,Tags=[{Key=Name,Value=volume{i},{Key=Project,Value=Roboshop}]"
+done
