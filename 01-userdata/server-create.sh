c18605f220370975c319c7a2eda447a45baf7bba
chaitu1 <chaitu143@gmail.com>
2024-06-21 20:39:54 +0530

    backend

--- 01-userdata/server-create.sh
+++ 01-userdata/server-create.sh
@@ -2,4 +2,5 @@ aws ec2 run-instances \
     --image-id ami-031d574cddc5bb371 \
     --instance-type t3.micro \
      --security-group-ids sg-0a88820d7b4d3ff2a \
-     --user-data file://script.txt
+     --user-data file://script.txt \
+     --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production},{Key=Name,Value=Frontend}]'
